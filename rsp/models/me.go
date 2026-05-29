a48c1ab8efa43d538c5c7f9ce75fe3bf334bce09
zigdon <dan@peeron.com>
2026-05-29 12:11:20 -0700

    Uh, the api returns json, not yaml whoops

--- rsp/models/me.go
+++ rsp/models/me.go
@@ -3,45 +3,45 @@ package models
 import (
 	"fmt"
 
-	"gopkg.in/yaml.v3"
+	"encoding/json"
 )
 
 type Notify struct {
-	Email bool `yaml:"email"`
-	Preferences map[string]bool `yaml:"preferences"`
-	Webhook bool `yaml:"webhook"`
+	Email bool `json:"email"`
+	Preferences map[string]bool `json:"preferences"`
+	Webhook bool `json:"webhook"`
 }
 
 type Replicant struct {
-	CreatedAt string `yaml:"created_at"`
-	CurrentLocation string `yaml:"current_location"`
-	CurrentLocationName string `yaml:"current_location_name"`
-	CurrentStar string `yaml:"current_star"`
-	CurrentStarName string `yaml:"current_star_name"`
-	DeviceCount int `yaml:"device_count"`
-	ExperiencePoints int `yaml:"experience_points"`
-	HostedDeviceCode string `yaml:"hosted_device_code"`
-	Name string `yaml:"name"`
-	ReplicantCode string `yaml:"replicant_code"`
+	CreatedAt string `json:"created_at"`
+	CurrentLocation string `json:"current_location"`
+	CurrentLocationName string `json:"current_location_name"`
+	CurrentStar string `json:"current_star"`
+	CurrentStarName string `json:"current_star_name"`
+	DeviceCount int `json:"device_count"`
+	ExperiencePoints int `json:"experience_points"`
+	HostedDeviceCode string `json:"hosted_device_code"`
+	Name string `json:"name"`
+	ReplicantCode string `json:"replicant_code"`
 }
 
 type Me struct {
-	BobnetChannels []string `yaml:"bobnet_channels"`
-	CreatedAt string `yaml:"created_at"`
-	Email string `yaml:"email"`
-	EmailVerified bool `yaml:"email_verified"`
-	ExperiencePointsTotal int `yaml:"experience_points_total"`
-	MessageNotify Notify `yaml:"message_notify"`
-	Name string `yaml:"name"`
-	Replicants []Replicant `yaml:"replicants"`
-	Status string `yaml:"status"`
-	Timezone string `yaml:"timezone"`
-	UnreadMessageCount int `yaml:"unread_message_count"`
+	BobnetChannels []string `json:"bobnet_channels"`
+	CreatedAt string `json:"created_at"`
+	Email string `json:"email"`
+	EmailVerified bool `json:"email_verified"`
+	ExperiencePointsTotal int `json:"experience_points_total"`
+	MessageNotify Notify `json:"message_notify"`
+	Name string `json:"name"`
+	Replicants []Replicant `json:"replicants"`
+	Status string `json:"status"`
+	Timezone string `json:"timezone"`
+	UnreadMessageCount int `json:"unread_message_count"`
 }
 
 func ParseMe(data []byte) (*Me, error) {
 	m := &Me{}
-	if err := yaml.Unmarshal(data, m); err != nil {
+	if err := json.Unmarshal(data, m); err != nil {
 		return nil, fmt.Errorf("Error parsing me: %v", err)
 	}
 
--- rsp/models/scan.go
+++ rsp/models/scan.go
@@ -3,55 +3,55 @@ package models
 import (
 	"fmt"
 
-	"gopkg.in/yaml.v3"
+	"encoding/json"
 )
 
 type Resources struct {
-	Carbon string `yaml:"carbon"`
-	Conductive string `yaml:"conductive"`
-	Rares string `yaml:"rares"`
-	Silicates string `yaml:"silicates"`
-	Structural string `yaml:"structural"`
-	Volatiles string `yaml:"volatiles"`
+	Carbon string `json:"carbon"`
+	Conductive string `json:"conductive"`
+	Rares string `json:"rares"`
+	Silicates string `json:"silicates"`
+	Structural string `json:"structural"`
+	Volatiles string `json:"volatiles"`
 }
 
 type Belts struct {
-	Density string `yaml:"density"`
-	Designation string `yaml:"designation"`
-	InnerRadiusAu float32 `yaml:"inner_radius_au"`
-	OuterRadiusAu float32 `yaml:"outer_radius_au"`
-	Resources Resources `yaml:"resources"`
+	Density string `json:"density"`
+	Designation string `json:"designation"`
+	InnerRadiusAu float32 `json:"inner_radius_au"`
+	OuterRadiusAu float32 `json:"outer_radius_au"`
+	Resources Resources `json:"resources"`
 }
 
 type Destination struct {
-	Designation string `yaml:"designation"`
-	DistanceAu float32 `yaml:"distanceAu"`
+	Designation string `json:"designation"`
+	DistanceAu float32 `json:"distanceAu"`
 }
 
 type Salvage struct {
-	Depleted bool `yaml:"depleted"`
-	Designation string `yaml:"designation"`
-	Location string `yaml:"location"`
-	Name string `yaml:"name"`
-	ResourcesAvailable []string `yaml:"resources_available"`
-	SalvageType string `yaml:"salvage_type"`
+	Depleted bool `json:"depleted"`
+	Designation string `json:"designation"`
+	Location string `json:"location"`
+	Name string `json:"name"`
+	ResourcesAvailable []string `json:"resources_available"`
+	SalvageType string `json:"salvage_type"`
 }
 
 type Planet struct {
-	Designation string `yaml:"designation"`
-	InHabitableZone bool `yaml:"in_habitable_zone"`
-	MoonCount int `yaml:"moon_count"`
-	Name string `yaml:"name"`
-	OrbitalDistanceAu float32 `yaml:"orbital_distance_au"`
-	Salvage []Salvage `yaml:"salvage"`
-	Scanned bool `yaml:"scanned"`
-	Type string `yaml:"type"`
+	Designation string `json:"designation"`
+	InHabitableZone bool `json:"in_habitable_zone"`
+	MoonCount int `json:"moon_count"`
+	Name string `json:"name"`
+	OrbitalDistanceAu float32 `json:"orbital_distance_au"`
+	Salvage []Salvage `json:"salvage"`
+	Scanned bool `json:"scanned"`
+	Type string `json:"type"`
 }
 
 type ScanReplicant struct {
-	LastActive string `yaml:"last_active"`
-	Location string `yaml:"location"`
-	ReplicantCode string `yaml:"replicant_code"`
+	LastActive string `json:"last_active"`
+	Location string `json:"location"`
+	ReplicantCode string `json:"replicant_code"`
 }
 
 type Position struct {
@@ -61,58 +61,58 @@ type Position struct {
 }
 
 type Star struct {
-	AgeMy float32 `yaml:"age_my"`
-	Color string `yaml:"color"`
-	Designation string `yaml:"designation"`
-    habitable_zone struct {
-		InnerAu float32 `yaml:"inner_au"`
-		OuterAu float32 `yaml:"outer_au"`
-	} `yaml:"habitable_zone"`
-	LuminositySolar float32 `yaml:"luminositysolar"`
-	MassSolar float32 `yaml:"mass_solar"`
-	Name string `yaml:"name"`
-	Position Position  `yaml:"position"`
-	SpectralType string `yaml:"spectral_type"`
-	TemperatureK int `yaml:"temperature_k"`
+	AgeMy float32 `json:"age_my"`
+	Color string `json:"color"`
+	Designation string `json:"designation"`
+    HabitableZone struct {
+		InnerAu float32 `json:"inner_au"`
+		OuterAu float32 `json:"outer_au"`
+	} `json:"habitable_zone"`
+	LuminositySolar float32 `json:"luminositysolar"`
+	MassSolar float32 `json:"mass_solar"`
+	Name string `json:"name"`
+	Position Position  `json:"position"`
+	SpectralType string `json:"spectral_type"`
+	TemperatureK int `json:"temperature_k"`
 }
 
 type Object struct {
-	ActivePlates int `yaml:"active_plates"`
-	CurrentProgress float32 `yaml:"current_progress"`
-	Designation string `yaml:"designation"`
-	ImpactEta string `yaml:"impact_eta"`
-	ImpactTarget string `yaml:"impact_target"`
-	ObjectType string `yaml:"object_type"`
-	OrbitalDistanceAu float32 `yaml:"orbital_distance_au"`
-	ProgressPct float32 `yaml:"progress_pct"`
-	RequiredStrength float32 `yaml:"required_strength"`
-	SizeClass string `yaml:"size_class"`
-	Status string `yaml:"status"`
+	ActivePlates int `json:"active_plates"`
+	CurrentProgress float32 `json:"current_progress"`
+	Designation string `json:"designation"`
+	ImpactEta string `json:"impact_eta"`
+	ImpactTarget string `json:"impact_target"`
+	ObjectType string `json:"object_type"`
+	OrbitalDistanceAu float32 `json:"orbital_distance_au"`
+	ProgressPct float32 `json:"progress_pct"`
+	RequiredStrength float32 `json:"required_strength"`
+	SizeClass string `json:"size_class"`
+	Status string `json:"status"`
 }
 
 type Scan struct {
-	ActiveLocationEvents []string `yaml:"active_location_events"`
-	asteroid_belt struct {
-		Belts []Belts `yaml:"belts"`
-		Present bool `yaml:"present"`
-	} `yaml:"asteroid_belt"`
-	EntryPoint string `yaml:"entry_point"`
-	LifeDetected bool `yaml:"life_detected"`
-	MiningBonusPct int `yaml:"mining_bonus_pct"`
-	outer_system struct {
-		Kuiper Destination `yaml:"kuiper"`
-		Oort Destination `yaml:"oort"`
-	} `yaml:"outer_system"`
-	Planets []Planet `yaml:"planets"`
-	Replicants map[string]ScanReplicant `yaml:"replicants"`
-	Star Star `yaml:"star"`
-	SystemObjects []Object `yaml:"system_objects"`
-	SystemTags []string `yaml:"system_tags"`
+	ActiveLocationEvents []string `json:"active_location_events"`
+	AsteroidBelt struct {
+		Belts []Belts `json:"belts"`
+		Present bool `json:"present"`
+	} `json:"asteroid_belt"`
+	EntryPoint string `json:"entry_point"`
+	LifeDetected bool `json:"life_detected"`
+	MiningBonusPct int `json:"mining_bonus_pct"`
+	OuterSystem struct {
+		Kuiper Destination `json:"kuiper"`
+		Oort Destination `json:"oort"`
+	} `json:"outer_system"`
+	Planets []Planet `json:"planets"`
+	Replicants map[string]ScanReplicant `json:"replicants"`
+	Star Star `json:"star"`
+	SystemObjects []Object `json:"system_objects"`
+	SystemTags []string `json:"system_tags"`
 }
 
 func ParseScan(data []byte) (*Scan, error) {
 	s := &Scan{}
-	if err := yaml.Unmarshal(data, s); err != nil {
+	if err := json.Unmarshal(data, s); err != nil {
 		return nil, fmt.Errorf("Error parsing scan: %v", err)
 	}
 
