4ee0c5a2ff03d5ae701633946ae24d9d63a6957d
zigdon <dan@peeron.com>
2026-06-01 17:38:33 -0700

    Start a new take on a tui

--- rsp/go.mod
+++ rsp/go.mod
@@ -12,9 +12,12 @@ require (
 
 require (
 	charm.land/huh/v2 v2.0.3 // indirect
+	github.com/76creates/stickers v1.5.0 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/catppuccin/go v0.2.0 // indirect
 	github.com/charmbracelet/colorprofile v0.4.3 // indirect
+	github.com/charmbracelet/lipgloss v0.13.0 // indirect
 	github.com/charmbracelet/ultraviolet v0.0.0-20260416155717-489999b90468 // indirect
 	github.com/charmbracelet/x/ansi v0.11.7 // indirect
 	github.com/charmbracelet/x/exp/ordered v0.1.0 // indirect
@@ -27,9 +30,11 @@ require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.23 // indirect
 	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
--- rsp/tui/helpers.go
+++ rsp/tui/helpers.go
@@ -1,101 +1,17 @@
 package tui
 
 import (
-	"embed"
-	"fmt"
-	"strings"
-
-	"text/template"
-
-	tea "charm.land/bubbletea/v2"
 	lg "charm.land/lipgloss/v2"
 )
 
-//// Templates
-
-//go:embed templates/*.tmpl
-var templates embed.FS
-
-func (m *Model) executeTmpl(name string, data any) string {
-	tmpl := t(name)
-	var s strings.Builder
-	if err := tmpl.Execute(&s, data); err != nil {
-		log("Error executing %q: %v", name, err)
+func box(style *lg.Style, w, h int, content string) string {
+	if style == nil {
+		s := lg.NewStyle()
+		style = &s
 	}
-	return screen(s.String())
-}
-
-func t(name string) *template.Template {
-	data, err := templates.ReadFile("templates/"+name+".tmpl")
-	if err != nil {
-		die("Can't read template %q: %v", name, err)
-	}
-	tmpl, err := template.New(name).Parse(string(data))
-	if err != nil {
-		die("Can't parse template %q: %v", name, err)
-	}
-	return tmpl
-}
-
-//// UI Elements
-type boxStyle int
-const (
-	titleStyle boxStyle = iota
-	headerStyle
-	logStyle
-	modalStyle
-)
-
-func box(style boxStyle, w, h int, tmpl string, args ...any) string {
-	if w == 0 { w = 40 }
-	st := lg.NewStyle().
-		Border(lg.RoundedBorder()).
-		PaddingLeft(3).
-		PaddingRight(3).
-		Width(w)
-	if h != 0 { st = st.Height(h) }
-	if style == titleStyle || style == headerStyle {
-		st = st.Align(lg.Center)
-	}
-	if style == logStyle {
-		st = st.Padding(0, 0, 2, 2)
-	}
-	return st.Render(fmt.Sprintf(tmpl, args...))
-}
-
-func screen(contents string) string {
-	return lg.NewStyle().
-		Border(lg.ThickBorder()).
-		Padding(0, 1, 2, 2).
-		Render(contents)
-}
-
-func background(w, h int) string {
-	return lg.NewStyle().
-		Width(w).
+	return style.
+		Border(lg.NormalBorder()).
 		Height(h).
-		Align(lg.Center).
-		Render("replicant.space")
-}
-
-var screenNotImplemented = &Screen{
-	Cursor: 0,
-	Render: func(*Model) *lg.Layer { return nil},
-}
-
-//// Menus
-type menuOption struct {
-	Text string
-	Action func(*Model) (*Model, tea.Cmd)
-	NextScreen screenID
-	Hotkey rune
-	BreakAfter bool
-}
-
-type menuData struct {
-	Title string
-	Header string
-	Footer string
-	Options []menuOption
-	Cursor int
+		Width(w).
+		Render(content)
 }
