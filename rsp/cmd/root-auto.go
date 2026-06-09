affdf4e890aa9a464cfce098f13ad547bb8a12d4
Dan Boger <dan@peeron.com>
2026-06-09 16:07:49 -0700

    Placeholder for future mining site automation

---
+++ rsp/cmd/root-auto.go
@@ -0,0 +1,17 @@
+package cmd
+
+// " huh, so each site keeps survey/mining controllers, and drones, so keep mining forever?"
+
+// Automatically set up persistent belt mining site
+// ami mining + mining drone
+// ami scanning + scanning drone
+// ftl relay
+// Tag with mine:SYSTEM-BELT-1
+// Build missing devices
+// Deliver built devices
+// Adopt drones to ami
+// Set ami policy
+
+// Collect resources
+// Pick site outside of the allowlist that has the most resources
+// Set ami transport to ferry stuff home
