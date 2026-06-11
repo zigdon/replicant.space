c4a97b38fc917cfc7e1a88586794266fd520ad42
Dan Boger <dan@peeron.com>
2026-06-11 16:07:07 -0700

    WIP: not really much progress on the auto-mine command

--- rsp/cmd/root-auto.go
+++ rsp/cmd/root-auto.go
@@ -1,5 +1,12 @@
 package cmd
 
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/zigdon/rsp/rest"
+)
+
 // " huh, so each site keeps survey/mining controllers, and drones, so keep mining forever?"
 
 // Automatically set up persistent belt mining site
@@ -15,3 +22,35 @@ package cmd
 // Collect resources
 // Pick site outside of the allowlist that has the most resources
 // Set ami transport to ferry stuff home
+
+var autoCmd = &cobra.Command{
+	Use:   "auto",
+	Short: "High level automation commands",
+}
+
+var autoMineCmd = &cobra.Command{
+	Use:   "mine",
+	Short: "Set up a new belt mine",
+	RunE:  autoMine,
+}
+
+func init() {
+	rootCmd.AddCommand(autoCmd)
+
+	autoCmd.PersistentFlags().Bool("dry_run", true, "When set, only describe what will be done")
+	autoCmd.PersistentFlags().String("owner", "zigdon-2", "Replicant responsible for printing new devices")
+
+	autoCmd.AddCommand(autoMineCmd)
+	autoMineCmd.Flags().StringP("location", "l", "", "Belt location to mine")
+	autoMineCmd.MarkFlagRequired("location")
+}
+
+func autoMine(cmd *cobra.Command, args []string) error {
+	locName, _ := cmd.Flags().GetString("location")
+	loc, err := rest.Location(locName)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Location: %s\n", loc.Location)
+	return nil
+}
